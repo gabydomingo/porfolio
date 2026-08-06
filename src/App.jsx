ead9ece06ee8f4578b3768a1555c7d136db1b84a
gabydomingo <86844990+gabydomingo@users.noreply.github.com>
2026-08-06 19:33:10 -0300

    Limpieza: saco componentes viejos y dependencias sin uso

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,3 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { NotFound } from "./pages/NotFound";
-import { Toaster } from "./components/ui/toaster";
-
-function App() {
- return (
-   <>
-   <Toaster />
-	 <BrowserRouter>
-	   <Routes>
-      <Route index element={<Home />} />
-      <Route path="*" element={<NotFound />}/>
-     </Routes>
-	 </BrowserRouter>
-   </>
- );
+export default function App() {
+  return null;
 }
-
-export default App
